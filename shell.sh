6fc621863fc1ef2a99847b579de1ed8f1ec37857
RcZo-2 <sb3mk1999@gmail.com>
2025-02-13 14:21:54 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker pull buildpack-deps:bookworm-scm
-docker build -f buildpack-deps_bookworm/Dockerfile -t buildpack-deps:bookworm
-docker build -f python_3.11/Dockerfile -t python:3.11
+docker build -f buildpack-deps_bookworm/Dockerfile -t buildpack-deps:bookworm .
+docker build -f python_3.11/Dockerfile -t python:3.11 .
